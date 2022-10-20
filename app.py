971d041c1b1f5dbb64e6cf770885bf3eeadb7972
Javad Pournosrat <pournosrat.javad@gmail.com>
2022-10-20 19:35:00 +0330

    characters not character

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 
-# every password has 16 character
+# every password has 16 characters
 
 English = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
